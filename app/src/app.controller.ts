1f748532750e6da76fed6cafbe39b1a128331be3
tonynin1 <daivietvonin1@gmail.com>
2026-04-17 00:33:54 +0700

    trigger build

--- app/src/app.controller.ts
+++ app/src/app.controller.ts
@@ -10,4 +10,9 @@ export class AppController {
   getHealth(): string {
     return this.appService.getHealth();
   }
+
+  @Get('test')
+  getTest(): string {
+    return this.appService.getTest();
+  }
 }
